8ed2d58dacdf3ec6da2f039d09b414f0f88960f7
Gabriel Nogueira <gab.nog94@gmail.com>
2020-07-29 13:30:16 -0300

    Changed border size

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 20;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
@@ -26,4 +26,5 @@ static unsigned int lineheight = 0;         /* -h option; minimum height of a me
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 0;
+
