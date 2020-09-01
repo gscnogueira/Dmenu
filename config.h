c714c7ffeeced9727ea1fd359b326644bcd08826
Gabriel Nogueira <gab.nog94@gmail.com>
2020-09-01 16:02:25 -0300

    Changed colors

--- config.h
+++ config.h
@@ -11,9 +11,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#6372a4", "#282a36" },
+	[SchemeNorm] = { "#6272a4", "#282a36" },
 	[SchemeSel] = { "#f8f8f2", "#bd93f9" },
-	[SchemeSelHighlight] = { "#282a36", "#bd93f9" },
+	[SchemeSelHighlight] = { "#6272a4", "#bd93f9" },
 	[SchemeNormHighlight] = { "#f1fa8c", "#282a36" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
