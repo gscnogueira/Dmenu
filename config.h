4611792b3f36efa0dfa32802eae5182a0262264a
Gabriel Nogueira <gab.nog94@gmail.com>
2020-12-30 12:15:21 -0300

    Changed theme

--- config.h
+++ config.h
@@ -12,9 +12,9 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#6272a4", "#282a36" },
-	[SchemeSel] = { "#f8f8f2", "#bd93f9" },
-	[SchemeSelHighlight] = { "#6272a4", "#bd93f9" },
-	[SchemeNormHighlight] = { "#f1fa8c", "#282a36" },
+	[SchemeSel] = { "#d8dee9", "#81a1c1" },
+	[SchemeSelHighlight] = { "#ebcb8b", "#81a1c1" },
+	[SchemeNormHighlight] = { "#d8dee9", "#2e3440" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
