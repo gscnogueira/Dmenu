4373e181f33f0cfcc4b5e68f7b0e0a4f628ce598
Gabriel Nogueira <gab.nog94@gmail.com>
2021-01-28 21:19:52 -0300

    Updated to dmenu 5.0

--- README.md
+++
@@ -1,9 +0,0 @@
-# My dmenu build
-
-## Patches:
-
-- [border](https://tools.suckless.org/dmenu/patches/border/);
-- [center](https://tools.suckless.org/dmenu/patches/center/);
-- [heighlight](https://tools.suckless.org/dmenu/patches/highlight/);
-- [line height](https://tools.suckless.org/dmenu/patches/line-height/);
-- [numbers](https://tools.suckless.org/dmenu/patches/numbers/);
--- config.h
+++ config.h
@@ -2,31 +2,22 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono:size=10"
+	"JetBrains Mono:size=10:style=Bold"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#81a1c1", "#2e3440" },
 	[SchemeSel] = { "#d8dee9", "#81a1c1" },
-	[SchemeSelHighlight] = { "#ebcb8b", "#81a1c1" },
-	[SchemeNormHighlight] = { "#d8dee9", "#2e3440" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 20;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
-
-/* Size of the window border */
-static const unsigned int border_width = 0;
-
